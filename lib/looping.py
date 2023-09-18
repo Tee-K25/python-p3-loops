e8463866799fcdbba2a3e60d676442f5d9136d80
Tony <tony.kiplagat@student.moringaschool.com>
2023-09-18 23:41:24 +0300

    Infinite loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown-=1
+        if countdown < 1:
+            print("Happy New Year!")
+            break
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [integer**2 for integer in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for n in range(1,101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+           
+        else:
+            print(n)
+
